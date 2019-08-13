e4f92e13a5bb30eff8cb7542641c8ccf5bc5ec6a
Jefferson Matheus <jefferson227@gmail.com>
2019-08-13 14:23:41 -0300

    Do the first api call to spotify
    Also 'axios' was added to the project

--- index.js
+++ index.js
@@ -2,13 +2,31 @@
 const http = require('http');
 const hostname = '127.0.0.1';
 const port = 3000;
+const axios = require('axios');
+
+//Spotify variables
+const baseUrl = 'https://api.spotify.com/v1/search?q=muse&type=artist';
 
 //Create HTTP server and listen on port 3000 for requests
 const server = http.createServer((req, res) => {
   //Set the response HTTP header with HTTP status and Content type
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n');
+  res.write('Hello World\n');
+
+  axios({
+    method: 'get',
+    url: baseUrl,
+    headers: {
+      Authorization: '<client id here>'
+    }
+  })
+    .then(response => {
+      res.write(response);
+    })
+    .catch(err => {
+      console.error(`There was an error during the API call: ${err}`);
+    });
 });
 
 //listen for request on port 3000, and as a callback function have the port listened on logged
