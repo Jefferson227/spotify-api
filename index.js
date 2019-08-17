b35003310bcf8fbe3fd7a7e0cf5c9020f73b47da
Jefferson Matheus <jefferson227@gmail.com>
2019-08-17 16:59:11 -0300

    Move axios instance

--- index.js
+++ index.js
@@ -11,17 +11,17 @@ dotenv.config();
 const baseUrl = 'https://api.spotify.com/v1/search?q=muse&type=artist';
 const token = process.env.CLIENT_ID;
 
-// Creating axios instance
-const instance = axios.create({
-  baseURL: baseUrl,
-  headers: { Authorization: `Bearer ${token}` }
-});
-
 app.get('/', function(req, res) {
   //Set the response HTTP header with HTTP status and Content type
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
   // res.write('Hello World\n');
+
+  // Creating axios instance
+  const instance = axios.create({
+    baseURL: baseUrl,
+    headers: { Authorization: `Bearer ${token}` }
+  });
 
   instance
     .get(baseUrl)
